6de027b0b7962814f8ce0730a5d9769e8c0324ee
wasd <kirshvedov13@gmail.com>
2024-02-18 14:52:04 +0200

    fix password encrypting in migration

--- user-microservice/src/main/resources/db/migration/V2__Insert_raws.sql
+++ user-microservice/src/main/resources/db/migration/V2__Insert_raws.sql
@@ -1,6 +1,6 @@
 insert into users (username, email, password)
-values ('user', 'user@user.com', '$2a$12$WOgvYv9MEZ44fHusrg4Wa.iTvfWBxvDcsoQcQWq7O1sj.NUCQ/CAC'),
-       ('admin', 'admin@admin.com', '$2a$12$CtTWVrBJuFGravhWXbkoa.J6vWG/ht/0CC5QoAPqm9L2vaJsWnxvi')
+values ('user', 'user@user.com', '{bcrypt}$2a$12$WOgvYv9MEZ44fHusrg4Wa.iTvfWBxvDcsoQcQWq7O1sj.NUCQ/CAC'),
+       ('admin', 'admin@admin.com', '{bcrypt}$2a$12$CtTWVrBJuFGravhWXbkoa.J6vWG/ht/0CC5QoAPqm9L2vaJsWnxvi')
 on conflict do nothing;
 
 insert into roles (name)
