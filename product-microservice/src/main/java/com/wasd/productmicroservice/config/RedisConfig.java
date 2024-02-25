0ef8085aa0c4f0af6fcdcd38859820f7d5477855
wasd <kirshvedov13@gmail.com>
2024-02-25 16:44:33 +0200

    update product service config

---
+++ product-microservice/src/main/java/com/wasd/productmicroservice/config/RedisConfig.java
@@ -0,0 +1,45 @@
+package com.wasd.productmicroservice.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+@Configuration
+@EnableRedisRepositories
+@Getter
+@Setter
+@ConfigurationProperties("spring.data.redis")
+public class RedisConfig {
+    private String host;
+    private int port;
+    private String password;
+    
+    @Bean
+    public RedisConnectionFactory connectionFactory() {
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
+        configuration.setHostName(host);
+        configuration.setPort(port);
+        configuration.setPassword(password);
+        return new JedisConnectionFactory(configuration);
+    }
+    
+    @Bean
+    public RedisTemplate<?, ?> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<?, ?> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        return template;
+    }
+    
+    @Bean
+    public RedisCacheManager redisCacheManager(RedisConnectionFactory connectionFactory) {
+        return RedisCacheManager.create(connectionFactory);
+    }
+}
