a8b0278f6c31e30f6eb4b6b07c186eaad4ce6d70
wasd <kirshvedov13@gmail.com>
2024-05-16 11:10:25 +0200

    feature: implement product service

--- product-microservice/src/main/java/com/wasd/productmicroservice/exception/common/NotFoundException.java
+++ product-microservice/src/main/java/com/wasd/productmicroservice/exception/common/NotFoundException.java
@@ -1,6 +1,6 @@
 package com.wasd.productmicroservice.exception.common;
 
-public class NotFoundException extends Exception {
+public class NotFoundException extends RuntimeException {
 
     public NotFoundException(Class<?> target) {
         super(String.format("%s not found", target.getSimpleName()));
--- product-microservice/src/main/java/com/wasd/productmicroservice/service/product/ProductServiceImpl.java
+++ product-microservice/src/main/java/com/wasd/productmicroservice/service/product/ProductServiceImpl.java
@@ -2,19 +2,33 @@ package com.wasd.productmicroservice.service.product;
 
 import com.wasd.productmicroservice.data.product.ProductRequest;
 import com.wasd.productmicroservice.data.product.ProductResponse;
+import com.wasd.productmicroservice.exception.common.NotFoundException;
+import com.wasd.productmicroservice.persistence.product.Product;
+import com.wasd.productmicroservice.persistence.product.ProductRepository;
+import com.wasd.productmicroservice.util.mapper.ProductMapper;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class ProductServiceImpl implements ProductService{
+public class ProductServiceImpl implements ProductService {
+    private final ProductMapper productMapper;
+    private final ProductRepository productRepository;
+
     @Override
     public ProductResponse findById(Long id) {
-        return null;
+        return productMapper.productToResponse(
+            productRepository.findById(id).orElseThrow(()
+                -> new NotFoundException(Product.class)));
     }
 
     @Override
+    @Transactional
     public ProductResponse create(ProductRequest request) {
-        return null;
+        Product newProduct = productMapper.requestToProduct(request);
+        productRepository.save(newProduct);
+        return productMapper.productToResponse(newProduct);
+
     }
 }
